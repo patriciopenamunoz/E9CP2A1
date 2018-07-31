34a93f0d1edb41ae7465beeb35bee2eec18cef68
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-07-30 21:20:21 -0400

    ADD ejercicio1.rb

---
+++ ejercicio1.rb
@@ -0,0 +1,14 @@
+# Se pide:
+
+# Crear una clase Table cuyo constructor reciba el nombre de la mesa y las
+# recaudaciones correspondientes de cada día.
+# Hint: El constructor debe recibir 5 argumentos.
+# Uno para el nombre de la mesa y los demás para cada recaudación
+# correspondiente a esa mesa.
+
+# Crear un método que permita leer el archivo e instanciar un objeto
+# de la clase Table por cada línea del archivo.
+
+# Crear métodos que permitan:
+# Conocer el mayor valor recaudado, por mesa, y el nombre de la mesa y
+# día corresponde (día 1, día 2, día 3 o día 4).
