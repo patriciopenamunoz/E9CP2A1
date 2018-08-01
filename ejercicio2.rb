84a4da87d31a473b1fbbf851eb55f89199aafe02
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-08-01 19:19:25 -0400

    Resolve ejercicio2.rb

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -16,3 +16,60 @@
 
 ### Ambos métodos deben levantar una excepción si la fecha recibida
 ### es >= 2018-01-01.
+
+require 'date'
+
+class CourseList
+  attr_reader :list
+  def initialize
+    @list = []
+  end
+
+  def add(course)
+    @list.push(course)
+  end
+
+  def es_2018_01_01?(date)
+    if date >= Date.new(2018, 1, 1)
+      puts 'ERROR: Fecha entregada es igual/mayor a 2018-01-01'
+      true
+    else
+      false
+    end
+  end
+
+  def beforeStartDate(date = Date.today)
+    date = date.is_a?(String) ? Date.parse(date) : date
+    unless es_2018_01_01?(date)
+      @list.select { |e| date >= e.start_date }
+           .each { |e| puts "#{e.name}: #{e.start_date}" }
+    end
+  end
+
+  def afterEndDate(date = Date.today)
+    date = date.is_a?(String) ? Date.parse(date) : date
+    unless es_2018_01_01?(date)
+      @list.select { |e| date <= e.end_date }
+           .each { |e| puts "#{e.name}: #{e.end_date}" }
+    end
+  end
+
+end
+
+class Course
+  attr_reader :name, :start_date, :end_date
+  def initialize(name, start_date, end_date)
+    @name = name
+    @start_date = Date.parse(start_date)
+    @end_date = Date.parse(end_date)
+  end
+end
+
+courses = CourseList.new
+file = File.open('cursos.txt', 'r')
+file.readlines.each { |line| courses.add(Course.new(*line.split(', '))) }
+file.close
+
+courses.beforeStartDate('2017-07-09')
+puts '---'
+courses.afterEndDate('2017-07-09')
