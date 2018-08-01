0f5b191ea218114722232bac2b9799fc87f111ed
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-08-01 19:21:39 -0400

    ADD ejercicio3.rb

---
+++ ejercicio3.rb
@@ -0,0 +1,18 @@
+# Crear una clase llamada Roulette.
+
+# Crear un método de instancia llamado play que reciba como argumento un
+# número al cual se está apostando. Luego debe tomar un valor -del array- al
+# azar y compararlo con el número al cual se apostó.
+
+# Crear un método de instancia que:
+
+# Debe almacenar el número generado al azar en un archivo
+# llamado roulette_history.txt
+
+# Si el número al cual se apostó corresponde al generado al azar entonces,
+# debe además almacenar ese número en un archivo llamado winners.txt
+
+# Crear un método que lea el archivo rouletter_history.txt y retorne el valor
+# que más ha generado la ruleta históricamente.
+
+r = (1..10).to_a
