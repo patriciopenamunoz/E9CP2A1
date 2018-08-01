0a9aa90de2e660b681aa57055f412c1c9a3ece03
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-08-01 18:38:33 -0400

    ADD ejercicio2.rb

---
+++ ejercicio2.rb
@@ -0,0 +1,18 @@
+# Se pide:
+
+# Crear una clase Course cuyo constructor reciba el nombre del curso y
+# las fechas de inicio y término.
+
+# Crear un método que permita leer el archivo y crear una instancia de la
+# clase Course por línea del archivo.
+
+# Crear métodos que permitan:
+
+## Saber qué cursos comienzan previo a una fecha entregada como argumento.
+
+## Saber qué cursos finalizan posterior a una fecha entregada como argumento.
+
+### En ambos el métodos argumento por defecto debe ser la fecha de hoy.
+
+### Ambos métodos deben levantar una excepción si la fecha recibida
+### es >= 2018-01-01.
